d749afd765216a5b1ad6ea68d45ebd56cce841cc
Olga Skopintseva <chocolate.olga.05@gmail.com>
2023-12-14 18:22:48 +0200

    feat: add solution for remove-from-list.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  while (l && l.value === k) {
+    l = l.next;
+  }
+
+  if (!l) {
+    return null;
+  }
+
+  let currentNode = l;
+  let prevNode = null;
+
+  while (currentNode !== null) {
+    if (currentNode.value === k) {
+      if (prevNode !== null) {
+        prevNode.next = currentNode.next;
+      }
+      currentNode = currentNode.next;
+    } else {
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+    }
+  }
+
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
